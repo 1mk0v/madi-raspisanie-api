13085d67d250a7e38d6316bd60518e135f55d363
d.potapchuk <d.potapchuk@sirena-travel.ru>
2023-07-18 17:14:06 +0300

    Add new modules methods

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime 
+
 request_url = 'https://raspisanie.madi.ru/tplan/tasks/{}'
 
 week_days = {
@@ -9,4 +11,27 @@ week_days = {
     "5":"Суббота",
     "6":"Воскресенье",
     "7":"Полнодневные занятия"
-} 
+}
+
+def get_current_year() -> int():
+
+    """Return current year"""
+
+    current_academic_year:int = int(datetime.today().strftime("%Y"))-2001 
+    current_month = int(datetime.today().strftime("%m"))
+    if current_month > 8:
+        current_academic_year -= 1
+
+    return current_academic_year
+
+
+def get_current_sem() -> int():
+
+    """Return current semester"""
+    
+    current_sem = 2
+    current_month = int(datetime.today().strftime("%m"))
+    if current_month > 8:
+        current_sem = 1
+
+    return current_sem
