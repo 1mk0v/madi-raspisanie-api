981570593142b9ea4edf0b7bc89a561f71f388d5
1mk0v <potapchuk01@mail.ru>
2023-08-10 19:05:30 +0300

    New methods

--- routers/groups.py
+++ routers/groups.py
@@ -1,29 +1,44 @@
 from MADI.main import  remove_spaces
 from MADI.my_requests import Groups as group_req
 from MADI.groups import Group
+from MADI.models import Group as Group_Model
 
+from typing import List
 from database.database import database
-from fastapi import APIRouter, HTTPException
-
+from database.schemas import group
+from fastapi import APIRouter, HTTPException, exceptions
+from requests import exceptions
 router = APIRouter(prefix='/group', tags=['Groups'])
 
 groups_req = group_req()
 
 
-@router.get('/')
+@router.get('/',
+            responses={
+                200:{
+                    "model":List[Group_Model]
+                },
+                404:{
+                    "model":None #TODO
+                }
+            })
 async def get_groups():
 
     """
     Returns all ID's of groups and their names
     """
-    data = dict()
+    
     try:
         html = await groups_req.get()
-    except Exception as error:
-        return HTTPException(404, detail=error.args[0])
+    except exceptions.ConnectionError:
+        query = group.select()
+        return await database.fetch_all(query)
+    except ValueError:
+        return HTTPException(404)
     
+    data = list()
     for tag in html:
-        data[tag['value']] = remove_spaces(tag.text)
+        data.append(Group_Model(id=tag['value'], value=tag.text))
     return data
 
 
@@ -36,9 +51,12 @@ async def get_group_schedule(id:int, sem:int = None, year:int = None, name:str =
 
     try:
         html = await groups_req.get_schedule(id, sem, year, name)
-    except Exception as error:
-        return HTTPException(404, detail=error.args[0])
+    except exceptions.ConnectionError:
+        return HTTPException(502)
+    except ValueError:
+        return HTTPException(404)
     
+
     data = Group.schedule(html=html, group_name=name)
     return data
 
@@ -49,10 +67,22 @@ async def get_group_exams(id: int, sem:int, year:int, name:str = None):
     """
     Returns JSON exams of group by id
     """
+
     try:
         html = await groups_req.get_exam(id,sem, year, name)
-    except Exception as error:
-        return HTTPException(404, detail=error.args[0])
+    except ValueError:
+        return HTTPException(404)
+    except exceptions.ConnectionError:
+        return HTTPException(502)
     
     data = Group.exam_schedule(html=html)
     return data
+
+
+@router.post('/add')
+async def add_group(name:str):
+    query = group.insert().values(name=name)
+    last_record_id = await database.execute(query)
+    return {
+        last_record_id:name
+    }
--- routers/teachers.py
+++ routers/teachers.py
@@ -1,16 +1,26 @@
 from MADI.my_requests import Teachers as teacher_reqs
+from MADI.models import Teacher as Teacher_model
 from MADI.teacher import Teacher
 from MADI.main import remove_spaces
-from typing import Annotated
-
-
+from typing import Annotated, List
+from requests import exceptions
+from database.database import database
+from database.schemas import teacher
 from fastapi import APIRouter, HTTPException, Path
 
 router = APIRouter(prefix='/teacher', tags=['Teachers'])
 
 teachers_req = teacher_reqs()
 
-@router.get('/')
+@router.get('/',
+            responses={
+                200:{
+                    "model":List[Teacher_model]
+                },
+                404:{
+                    "model":None #TODO
+                }
+            })
 async def get_all_teachers(
 # sem: Annotated[int, Path(ge=1, le=2)] = get_current_sem(),
 # year: Annotated[int, Path(ge=19, le=get_current_year())] = get_current_year():
@@ -18,16 +28,26 @@ async def get_all_teachers(
     year: int
     ):
 
-    """Returns the id and names of teachers in MADI"""
+    """
+    Returns the id and names of teachers in MADI
+    """
 
-    html = await teachers_req.get(year, sem)
+    try:
+        html = await teachers_req.get(year, sem)
+    except exceptions.ConnectionError:
+        query = teacher.select()
+        return await database.fetch_all(query)
+    except ValueError:
+        return HTTPException(404)
+    
     if len(html) == 0:
         raise HTTPException(404, detail=html.text)
     
-    data = dict()
-    for teacher in html:
-        if int(teacher['value']) > 0:
-            data[teacher['value']] = remove_spaces(teacher.text)
+    data = list()
+    for element in html:
+        if int(element['value']) > 0:
+
+            data.append(Teacher_model(id=element['value'], value=remove_spaces(element.text)))
 
     return data
 
@@ -39,8 +59,13 @@ async def get_teacher_schedule(id: int,
 
     """Returns JSON teacher schudule"""
 
-    html = await teachers_req.get_schedule(id, year, sem)
-
+    try:
+        html = await teachers_req.get_schedule(id, year, sem)
+    except ValueError:
+        return HTTPException(404)
+    except exceptions.ConnectionError:
+        return HTTPException(502)
+    
     data = Teacher.get_schedule(html=html)
 
     return data
@@ -51,15 +76,26 @@ async def get_teacher_exam(id: int,
                            sem: int,
                            year: int):
 
-    """Returns JSON exam of teacher by id and year"""
+    """
+    Returns JSON exam of teacher by id and year
+    """
 
-    html = await teachers_req.get_exam(id, year, sem)
+    try:
+        html = await teachers_req.get_exam(id, year, sem)
+    except ValueError:
+        return HTTPException(404)
+    except exceptions.ConnectionError:
+        return HTTPException(502)
 
     data = Teacher.exam_schedule(html=html)
 
     return data
 
 
-# @router.post('/{id}')
-# async def add_teacher(id:int):
-#     pass
+@router.post('/add')
+async def add_teacher(name:str):
+    query = teacher.insert().values(name=name)
+    last_record_id = await database.execute(query)
+    return {
+        last_record_id:name
+    }
