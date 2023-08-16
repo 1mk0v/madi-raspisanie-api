6f59a25e0fea7e6c9f292d079163069864d10a17
1mk0v <potapchuk01@mail.ru>
2023-08-16 18:59:24 +0300

    Add run app

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from routers import departments, groups, teachers
 from database import database
 
@@ -21,3 +20,6 @@ app.include_router(teachers.router)
 app.include_router(groups.router)
 # app.include_router(departments.router)
 app.include_router(database.router)
+
+if __name__ == '__main__':
+    uvicorn.run(app) 
