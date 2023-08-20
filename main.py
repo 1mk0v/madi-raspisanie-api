bbc3663cab20b73b0071fef1572c5bd88704ef68
d.potapchuk <d.potapchuk@sirena-travel.ru>
2023-08-20 23:14:32 +0300

    Add new info to docs

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import departments, groups, teachers, schedule
+from routers import db_methods, departments, groups, teachers
 from database import database
 
-app = FastAPI(title='MADI ASU Terminal API')
+app = FastAPI(
+    title='MADI SCHEDULE API',
+    description="""
+MADI SCHEDULE API
+
+## Teachers
+
+* You can get **list of teachers info**.\n
+* You can get **list of teacher schedule info**.\n
+* You can add **teacher info**.\n
+* You can delete **teacher info**.\n
+
+## Groups
+
+* You can get **list of groups info**.\n
+* You can get **list of group schedule info**.\n
+* You can add **group info**.\n
+* You can delete **group info**.\n""",
+    contact={
+        "name": "Potapchuk Danila Antonovich",
+        "url": "https://t.me/nivicki",
+        "email": "potapchuk01@mail.ru",
+    },
+)
 
 origins = ["*"]
 
@@ -18,7 +41,7 @@ app.add_middleware(
 
 app.include_router(teachers.router)
 app.include_router(groups.router)
-app.include_router(schedule.router)
+app.include_router(db_methods.router)
 # app.include_router(departments.router)
 app.include_router(database.router)
 
