911d5824e3398bbd38478354ed8d88b366b3fffa
1mk0v <potapchuk01@mail.ru>
2023-12-26 23:48:02 +0300

    Refactoring

--- src/auditoriums/__init__.py
+++ src/auditoriums/__init__.py
@@ -1,13 +1,52 @@
 from madi import RaspisanieDepartments
 from database.interfaces import Interface 
 from database.schemas import auditorium
-from models import Essence as AuditoriumModel
-
+from models import Community as AuditoriumModel, LessonInfo, List
+from schedule import router as scheduleRouter
+import datetime
+import utils
 class Auditoriums:
 
-    def __init__(self) -> None:
+    def __init__(self, dep_id = 61) -> None:
         self.auditorium = Interface(AuditoriumModel, auditorium)
         self.raspisanieDepartments = RaspisanieDepartments()
+        self.departmentId = dep_id
+        self.days = {
+            0: 'Понедельник',
+            1: 'Вторник',
+            2: 'Среда',
+            3: 'Четверг',
+            4: 'Пятница',
+            5: 'Суббота',
+            6: 'Воскресенье'
+        }
+        self.weekdayTypes = {
+            1: ['Числитель', 'Числ. 1 раз в месяц', 'Еженедельно'],
+            0: ['Знаменатель', 'Знам. 1 раз в месяц', 'Еженедельно']
+        }
 
-    def getAllFreeAuditoriums(self):
-        return self.raspisanieDepartments.get()
+    async def getAllAuditoriums(self):
+        return await self.auditorium.getByColumn('department_id', self.departmentId)
+    
+    async def getBusyAuditoriums(self):
+        busyAuditoriums = list()
+        currentDatetime = datetime.datetime.now()
+        schedules:List[LessonInfo] = (await scheduleRouter.getDepartmentSchedule(id = self.departmentId)).data
+        for schedule in schedules:
+            if schedule.weekday == self.days[currentDatetime.today().weekday()] \
+            and schedule.date.friequency in self.weekdayTypes[currentDatetime.isocalendar().week%2]:
+                if currentDatetime.time() > schedule.date.time.start \
+                and currentDatetime.time() < schedule.date.time.end:
+                    busyAuditoriums.append(schedule)
+        return busyAuditoriums
+    
+    async def getFreeAuditoriums(self):
+        currentDatetime = datetime.datetime.now()
+        schedules:List[LessonInfo] = (await scheduleRouter.getDepartmentSchedule(id = self.departmentId,
+                                                                                 sem=utils.get_current_sem(),
+                                                                                 year=utils.get_current_year())).data
+        for schedule in schedules:
+            if schedule.weekday == self.days[currentDatetime.today().weekday()] \
+            and schedule.date.friequency in self.weekdayTypes[currentDatetime.isocalendar().week%2]:
+                if currentDatetime.time() > schedule.date.time.end:
+                    print(currentDatetime.time(), schedule, '\n')
--- src/auditoriums/router.py
+++ src/auditoriums/router.py
@@ -1,75 +1,30 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException
 from database.interfaces import Interface
 from madi import RaspisanieDepartments
 from models import Community
 from database.schemas import auditorium
-from bridges import madi
-from requests import exceptions
-import datetime
-import dependencies
-from schedule import router as scheduleRouter
-from models import LessonInfo, List
+from . import Auditoriums
 
 router = APIRouter(prefix='/auditoriums', tags=['Auditoriums'])
 raspisanieDepartments = RaspisanieDepartments()
 auditoriumsTable = Interface(schema=auditorium, model=Community)
 
 @router.get(
-        "/department/{id}",
+        "/asu",
         summary = "GET department auditoriums",
-        # description="By default get actual schedule for teacher, but you can get old schedule",
 )
-async def getDepartmentAuditoriums(
-    id:int,
-    sem = Depends(dependencies.current_sem),
-    year = Depends(dependencies.current_year)
-):  
-    auditoriums = list()
-    try:
-        html = await raspisanieDepartments.get_auditoriums(id, sem, year)
-        bridge = madi.MADIBridge(html)
-        for i in bridge.generateLessons():
-            if i != None and i.auditorium not in auditoriums: auditoriums.append(i.auditorium)
-        return auditoriums
-    except (exceptions.ConnectionError, ValueError) as error:
-        raise HTTPException(404, detail=error.args[0])
+async def getDepartmentAuditoriums():
+    auditorium = Auditoriums()
+    return await auditorium.getAllAuditoriums()
 
 
 @router.get(
-        "/department/{id}/busy",
+        "/asu/free",
         summary = "GET free departments auditoriums" 
 )
-async def getFreeDepartmentAuditoriums(
-    id:int,
-    sem = Depends(dependencies.current_sem),
-    year = Depends(dependencies.current_year),
-):
-    days = {
-        0: 'Понедельник',
-        1: 'Вторник',
-        2: 'Среда',
-        3: 'Четверг',
-        4: 'Пятница',
-        5: 'Суббота',
-        6: 'Воскресенье'
-    }
-    weekdayType = {
-        1: ['Числитель', 'Числ. 1 раз в месяц', 'Еженедельно'],
-        0: ['Знаменатель', 'Знам. 1 раз в месяц', 'Еженедельно']
-    }
-    currentDatetime = datetime.datetime.now()
-    busyAuditoriums = list()
-    try:
-        schedules:List[LessonInfo] = (await scheduleRouter.getDepartmentSchedule(id, sem=sem, year=year)).data
-        for schedule in schedules:
-            if schedule.weekday == days[currentDatetime.today().weekday()] \
-            and schedule.date.friequency in weekdayType[currentDatetime.isocalendar().week%2]:
-                if currentDatetime.time() > schedule.date.time.start \
-                and currentDatetime.time() < schedule.date.time.end:
-                    busyAuditoriums.append(schedule.auditorium)
-        return busyAuditoriums
-    except Exception as error:
-        print(error, 'error')
+async def getFreeDepartmentAuditoriums():
+    auditorium = Auditoriums()
+    return await auditorium.getFreeAuditoriums()
 
 
 @router.post(
