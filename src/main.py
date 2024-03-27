241f83ca1a8d32e3f652ea1c3b37d278ad38e8ad
1mk0v <potapchuk01@mail.ru>
2024-03-27 19:10:17 +0300

    Add middlware and docs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import config
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from groups.router import router as group_router
 from teachers.router import router as teacher_router
@@ -8,13 +9,20 @@ from department.router import router as department_router
 from auditoriums.router import router as auditoriumRouter
 
 app = FastAPI(
-    title='MADI SCHEDULE API',
+    title='API Расписание МАДИ',
     contact={
-        "name": "Potapchuk Danila Antonovich",
+        "name": "Данила Антонович",
         "url": "https://t.me/nivicki",
         "email": "potapchuk01@mail.ru",
     },
+    version='0.2.1',
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+
 )
+app.openapi_version = "3.0.2"
 
 origins = ["*"]
 
@@ -26,8 +34,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    print("Time took to process the request and return response is {} sec".format(time.time() - start_time))
+    return response
+
 app.include_router(auditoriumRouter)
 app.include_router(department_router)
 app.include_router(group_router)
 app.include_router(teacher_router)
 app.include_router(events_router)
+
+
+@app.on_event('startup')
+async def startup_event():
+    from database.database import create_database
+    create_database()
